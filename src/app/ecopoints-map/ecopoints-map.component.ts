ae062a2a815ebdb182579ecb5535e457b05eb1c6
Luis José Sánchez <luisjoseprofe@gmail.com>
2017-04-22 23:20:46 +0200

    all ecopoints markers are shown

--- src/app/ecopoints-map/ecopoints-map.component.ts
+++ src/app/ecopoints-map/ecopoints-map.component.ts
@@ -1,3 +1,5 @@
+import { EcoPoint } from './../model/EcoPoint';
+import { EcopointsService } from './../model/ecopoints.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,9 +12,12 @@ export class EcopointsMapComponent implements OnInit {
   lat: number = 36.693466;
   lon: number = -4.440807;
 
-  constructor() { }
+  ecoPoints: EcoPoint[];
+
+  constructor(public ecoPointsService: EcopointsService) { }
 
   ngOnInit() {
+    this.ecoPoints = this.ecoPointsService.allEcoPoints();
   }
 
 }
